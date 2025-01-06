08fc481535fe51c4c8c75ddaf549eecd6b2071a5
Eric J. Sudadyo <eric.sudadyo@telkom.co.id>
2025-01-06 13:04:25 +0700

    Update better solution for problem MySQL Capital Gainloss

--- SQL/Medium/capital_gainloss.sql
+++ SQL/Medium/capital_gainloss.sql
@@ -2,11 +2,9 @@
 
 -- # Write your MySQL query statement below
 
-SELECT stock_name, SUM(
-    CASE
-        WHEN operation='Buy' THEN -price
-        ELSE price
-    END
-) AS capital_gain_loss
+SELECT
+  stock_name,
+  SUM((operation = 'Sell') * price) - SUM((operation = 'Buy') * price)
+    AS capital_gain_loss
 FROM Stocks
 GROUP BY stock_name;
